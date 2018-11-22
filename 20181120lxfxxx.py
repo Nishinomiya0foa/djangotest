a1d29c0d77fb74c6f9c371e21e1442897cdefbe8
chen in company <908294800@qq.com>
2018-11-22 11:47:32 +0800

    lxfxxx modify

--- 20181120lxfxxx.py
+++ 20181120lxfxxx.py
@@ -93,3 +93,33 @@ voc._height = 768
 print(voc._width, voc._height)
 
 
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'Student name is {}'.format(self.name)
+
+s = Student('Liu')
+print(s)
+
+from enum import Enum
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr'))
+
+for name,member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+
+class Gender(Enum):
+    Male = 0
+    Female = 1
+
+class Student(object):
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender
+
+    def show(self):
+        return '{}\'s gender is {}'.format(self.name, self.gender)
+
+s = Student('Chen', 0)
+print(s.show())
