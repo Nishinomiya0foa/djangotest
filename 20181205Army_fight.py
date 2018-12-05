14d9e2a6a35606a895b36280b5343966c827ef35
chen in company <908294800@qq.com>
2018-12-05 18:00:10 +0800

    warrior fight+

--- 20181205Army_fight.py
+++ 20181205Army_fight.py
@@ -1,4 +1,7 @@
 # Army fight with another
+import logging
+logging.basicConfig(level=logging.INFO)
+
 
 class Warrior(object):
     def __init__(self, health=50, attack=5):
@@ -18,11 +21,15 @@ class Army(object):
     def add_units(self, _soldier='Warrior', _count=0):
         self._soldier = _soldier
         self._count = _count
+        return self._soldier,self._count
 
 def fight():
     pass
 
 my_army = Army()
 my_army.add_units('Warrior', 3)
+logging.info(my_army.add_units('Warrior', 3))
 em_army = Army()
 em_army.add_units('Knight', 4)
+logging.info(my_army.add_units('Knight', 4))
+
